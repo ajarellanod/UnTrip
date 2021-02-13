62017325eb43ccf051090dda008a7923327967e4
Alex Arellano <ajarellanod@gmail.com>
2021-02-13 09:24:16 -0400

    Feat: creado todos los modelos bases

--- untrip/backend/models.py
+++ untrip/backend/models.py
@@ -1,43 +1,107 @@
 from django.db import models
+from django.db.models import Q, F
 
 
-class Horario(models.Model):
-    salida = models.TimeField()
+class Ticket(models.Model):
+
+    pasajero = models.CharField(max_length=100)
+
+    asiento = models.ForeignKey(
+        'Asiento',
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
 
 
 class Bus(models.Model):
-    pass
+
+    DISPONIBLE = 1
+    EN_RUTA = 2
+    
+    ESTADOS = (
+        (DISPONIBLE, "Disponible"),
+        (EN_RUTA, "En Ruta")
+    )
+
+    estado = models.IntegerField(
+        choices=ESTADOS, default=DISPONIBLE
+    )
+
+    chofer = models.CharField(max_length=100)
+
+    capacidad = models.IntegerField()
 
 
 class Trayecto(models.Model):
-    nombre = models.CharField()
+    
+    nombre = models.CharField(max_length=100)
+
+    duracion = models.DurationField()
+
+
+class Asiento(models.Model):
+
+    DISPONIBLE = 1
+    RESERVADO = 2
+
+    identificador = models.IntegerField()
+
+    ESTADOS = (
+        (DISPONIBLE, "Disponible"),
+        (RESERVADO, "Reservado")
+    )
+
+    ruta = models.ForeignKey(
+        'Ruta',
+        on_delete=models.CASCADE,
+        related_name="asientos"
+    )
+
+    estado = models.IntegerField(choices=ESTADOS, default=DISPONIBLE)
 
 
 class Ruta(models.Model):
-    
+
     trayecto = models.ForeignKey(
         'Trayecto',
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="rutas"
     )
-    
-    horario = models.ForeignKey(
-        'Horario',
-        on_delete=models.CASCADE
-    )
-    
+
     bus = models.ForeignKey(
         'Bus',
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="rutas"
     )
 
+    salida = models.DateTimeField()
+
+    llegada = models.DateTimeField()
+
+    def gen_asientos(self):
+        """ Genera Asiento para la ruta creada """
+        for identificador in range(self.bus.capacidad):
+            asiento = Asiento(ruta=self, identificador=identificador)
+            asiento.save()
 
 
-    
-    
-    
+    def is_valid_bus(self):
+        """ Valida que el Bus tenga disponibilidad """
+        query = Ruta.objects.filter(
+            Q(bus=self.bus),
+            Q(llegada__gte=self.salida),
+        )
+
+        if query.first():
+            return False
+        else:
+            return True
 
 
-    
-    
-    
-    
+    def save(self, *args, **kwargs):
+        if self.is_valid_bus():
+            self.llegada = self.salida + self.trayecto.duracion
+            super().save(*args, **kwargs)
+            self.gen_asientos()
+        else:
+            raise ValueError("Bus no disponible en esa fecha")
--- untrip/untrip/settings.py
+++ untrip/untrip/settings.py
@@ -5,7 +5,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '%u-kg0lms5wcsg5p2@n0)c-+04evz_0@(+@r$^*=%&)^o6wuhk'
@@ -69,18 +68,20 @@ WSGI_APPLICATION = 'untrip.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'untripdb',
+        'USER': 'untrip',
+        'PASSWORD': 'untrip',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
 
 # Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -99,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -113,6 +113,5 @@ USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
