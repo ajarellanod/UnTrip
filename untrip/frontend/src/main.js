f2772e28c81d0ea021e5b107ba7440450982bf89
Alex Arellano <ajarellanod@gmail.com>
2021-02-14 16:07:15 -0400

    fix: fallas en frontend app

--- untrip/frontend/src/main.js
+++ untrip/frontend/src/main.js
@@ -2,8 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
 
 createApp(App)
   .use(store)
   .use(router)
+  .use(Buefy)
   .mount("#app");
--- untrip/frontend/src/views/Home.vue
+++ untrip/frontend/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>We Are Testing VueJS</h1>
+    <Navbar />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Navbar from "@/components/Navbar.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Navbar
   }
 };
 </script>
